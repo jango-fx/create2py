0ffffd8ac02975c8d3ea2c8010a6a3a8f5ca47c5
jango-fx <felix.groll@gmail.com>
2017-07-11 11:52:26 +0200

    updated motor example

--- example_motor.py
+++ example_motor.py
@@ -3,29 +3,42 @@ import time             # sleep function
 import os               # filesystem
 
 dir = os.path.dirname(__file__)
-configFile = dir+'/config.json'
+configFile = dir+'/create2api/config.json'
 
 print '//// Start Connection ////////////////////////////////////////////'
-bot = create2api.Create2(configFile)
+bot = create2api.Create2('/dev/cu.usbserial-DA01NWTC', 115200, configFile)
 bot.start()
 bot.full()
 
 print '  // Forward'
 bot.drive_straight(200)
-time.sleep(1)
+time.sleep(0.5)
 
 print '  // Backward'
 bot.drive_straight(-200)
-time.sleep(1)
+time.sleep(0.5)
 
-print '  // Circle'
+print '  // Circle Left'
 velocity = 200
-radius = 500
+radius = 50
 bot.drive(velocity, radius)
-time.sleep(1)
+time.sleep(0.75)
+
+print '  // Circle Backward'
+velocity = -200
+radius = 50
+bot.drive(velocity, radius)
+time.sleep(0.75)
+
+print '  // Circle Right'
+velocity = 200
+radius = -50
+bot.drive(velocity, radius)
+time.sleep(0.75)
 
 print '  // Stop'
 bot.drive_straight(0)
+time.sleep(0.2)
 
 bot.reset()
 bot.destroy()
